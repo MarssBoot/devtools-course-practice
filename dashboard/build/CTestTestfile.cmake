7c348b512ee73d7c642e522deb539d24a7208949
MarssBoot <whitenoize2001@yandex.ru>
2022-03-30 18:09:44 +0300

    remove files from git

---
+++ dashboard/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/white/source/repos/Dev_tools/devtools-course-practice
+# Build directory: C:/Users/white/source/repos/Dev_tools/devtools-course-practice/dashboard/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("3rdparty/gtest")
+subdirs("modules/complex-number")
